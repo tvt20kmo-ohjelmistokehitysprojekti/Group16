6537bd55a823ddc65a3656835d90f06c2b3a3d11
Aino-SiskoAuvinen <64329478+Aino-SiskoAuvinen@users.noreply.github.com>
2020-12-13 20:57:21 +0200

    saldokysely

---
+++ RestApi/application/controllers/api/Saldo.php
@@ -0,0 +1,86 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+// This can be removed if you use __autoload() in config.php OR use Modular Extensions
+/** @noinspection PhpIncludeInspection */
+require APPPATH . 'libraries/REST_Controller.php';
+
+/**
+ * This is an example of a RestApi based on PHP and CodeIgniter 3.
+ * 
+ *
+ * @package         CodeIgniter
+ * @subpackage      Rest Server
+ * @category        Controller
+ * @CreditLimit          Pekka Alaluukas (edited the version made by Phil Sturgeon & Chris Kacerguis)
+ * @license         MIT
+ * @link            https://github.com/chriskacerguis/codeigniter-restserver
+ **/
+class Saldo extends REST_Controller {
+
+    function __construct()
+    {
+        //enable cors
+        header('Access-Control-Allow-Origin: *');
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+        // Construct the parent class
+        parent::__construct();
+
+        $this->load->model('Saldo_model');
+    }
+
+   
+
+   function index_get()
+    {
+        // Account from a data store e.g. database  
+
+        $id = $this->input->get('id');
+
+        // If the id parameter doesn't exist return all Accounts
+        if ($id === NULL)
+        {
+            $Saldo=$this->Saldo_model->get_Saldo(NULL);
+            // Check if the Account data store contains Account (in case the database result returns NULL)
+            if ($Saldo)
+            {
+                // Set the response and exit
+                $this->response($Saldo, REST_Controller::HTTP_OK); // OK (200) being the HTTP response code
+            }
+            else
+            {
+                // Set the response and exit
+                $this->response([
+                    'status' => FALSE,
+                    'message' => 'No Account were found'
+                ], REST_Controller::HTTP_NOT_FOUND); // NOT_FOUND (404) being the HTTP response code
+            }
+        }
+
+         // Find and return a single record for a particular Account.
+        else {
+            // Validate the id.
+            if ($id <= 0)
+            {
+                // Invalid id, set the response and exit.
+                $this->response(NULL, REST_Controller::HTTP_BAD_REQUEST); // BAD_REQUEST (400) being the HTTP response code
+            }
+
+            // Get the Account from the database, using the id as key for retrieval.
+            $Saldo=$this->Saldo_model->get_Saldo($id);
+            if (!empty($Saldo))
+            {
+                $this->set_response($Saldo, REST_Controller::HTTP_OK); // OK (200) being the HTTP response code
+            }
+            else
+            {
+                $this->set_response([
+                    'status' => FALSE,
+                    'message' => 'Account could not be found'
+                ], REST_Controller::HTTP_NOT_FOUND); // NOT_FOUND (404) being the HTTP response code
+            }
+        }
+
+    }
+}
